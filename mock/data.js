195b6b5e5768095a05349e8d9deadee9015c634e
ashalfarhan8 <ashalfarhan8@gmail.com>
2021-01-30 02:11:53 +0700

    hero bg and service data

---
+++ mock/data.js
@@ -0,0 +1,37 @@
+export const serviceData = [
+  {
+    title: "Search doctor",
+    desc:
+      "Choose your doctor from thousands of specialist, general, and trusted hospitals",
+    icon: "/icons/service1.svg",
+  },
+  {
+    title: "Online pharmacy",
+    desc:
+      "Buy  your medicines with our mobile application with a simple delivery system",
+    icon: "/icons/service2.svg",
+  },
+  {
+    title: "Consultation",
+    desc:
+      "Free consultation with our trusted doctors and get the best recomendations",
+    icon: "/icons/service3.svg",
+  },
+  {
+    title: "Details info",
+    desc:
+      "Free consultation with our trusted doctors and get the best recomendations",
+    icon: "/icons/service4.svg",
+  },
+  {
+    title: "Emergency care",
+    desc:
+      "You can get 24/7 urgent care for yourself or your children and your lovely family",
+    icon: "/icons/service5.svg",
+  },
+  {
+    title: "Tracking",
+    desc: "Track and save your medical history and health data ",
+    icon: "/icons/service6.svg",
+  },
+];
