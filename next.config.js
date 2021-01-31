79b17a988e19008ef8542628c4859afe9010f04d
ashalfarhan8 <ashalfarhan8@gmail.com>
2021-02-01 03:10:12 +0700

    add plugin for svg

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
-module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
-      },
-      use: ["@svgr/webpack"],
-    });
+const withReactSvg = require("next-react-svg");
+const path = require("path");
 
+module.exports = withReactSvg({
+  include: path.resolve(__dirname, "public/icons"),
+  webpack(config, options) {
     return config;
   },
-};
+});
