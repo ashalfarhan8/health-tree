a7b75aa9a93ed519b20aa5a71d8d8ced0536251b
ashalfarhan8 <ashalfarhan8@gmail.com>
2021-02-04 00:38:48 +0700

    add service data

--- mock/data.js
+++ mock/data.js
@@ -3,35 +3,35 @@ export const serviceData = [
     title: "Search doctor",
     desc:
       "Choose your doctor from thousands of specialist, general, and trusted hospitals",
-    icon: "/icons/service1.svg",
+    icon: "service1",
   },
   {
     title: "Online pharmacy",
     desc:
       "Buy  your medicines with our mobile application with a simple delivery system",
-    icon: "/icons/service2.svg",
+    icon: "service2",
   },
   {
     title: "Consultation",
     desc:
       "Free consultation with our trusted doctors and get the best recomendations",
-    icon: "/icons/service3.svg",
+    icon: "service3",
   },
   {
     title: "Details info",
     desc:
       "Free consultation with our trusted doctors and get the best recomendations",
-    icon: "/icons/service4.svg",
+    icon: "service4",
   },
   {
     title: "Emergency care",
     desc:
       "You can get 24/7 urgent care for yourself or your children and your lovely family",
-    icon: "/icons/service5.svg",
+    icon: "service5",
   },
   {
     title: "Tracking",
     desc: "Track and save your medical history and health data ",
-    icon: "/icons/service6.svg",
+    icon: "service6",
   },
 ];
