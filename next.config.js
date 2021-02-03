9efa98ff38c5f15f141b913a692dfdee60c72f6b
ashalfarhan8 <ashalfarhan8@gmail.com>
2021-02-04 02:42:13 +0700

    add compose plugins for multiple configuration

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
+const withPlugins = require("next-compose-plugins");
 const withReactSvg = require("next-react-svg");
 const path = require("path");
 
-module.exports = withReactSvg({
+const nextConfig = {
+  images: {
+    domains: [`randomuser.me`],
+  },
+};
+
+const svgPlugins = withReactSvg({
   include: path.resolve(__dirname, "public/icons"),
   webpack(config, options) {
     return config;
   },
 });
+
+module.exports = withPlugins([[svgPlugins]], nextConfig);
